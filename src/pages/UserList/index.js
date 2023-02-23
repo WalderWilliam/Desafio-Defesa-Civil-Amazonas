493190fa1bc982acaa4a7c129fd694a333b179cb
Walder William <walderwilliam@gmail.com>
2023-02-22 20:22:33 -0400

    UserList Update

--- src/pages/UserList/index.js
+++ src/pages/UserList/index.js
@@ -1,13 +1,61 @@
 import React from 'react'
+import {
+  View,
+  Text,
+  StyleSheet,
+  TextInput,
+  TouchableOpacity
+} from 'react-native'
 
-import { View, Text } from 'react-native'
+import * as Animatable from 'react-native-animatable'
 
 export default function UserList() {
+
   return (
-    <View>
-      <View>
-        <Text>Hello world ok</Text>
-      </View>
+    <View style={styles.container}>
+      <Animatable.View
+        delay={500}
+        animation="fadeInLeft"
+        style={styles.containerHeader}
+      >
+        <Text style={styles.message}>User Screen</Text>
+      </Animatable.View>
+
+      <Animatable.View
+        delay={600}
+        animation="fadeInUp"
+        style={styles.containerForm}
+      >
+      </Animatable.View>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#2D265C'
+  },
+
+  containerHeader: {
+    marginTop: '14%',
+    marginBottom: '8%',
+    paddingStart: '5%'
+  },
+
+  message: {
+    fontSize: 28,
+    fontWeight: 'bold',
+    color: '#fff',
+  },
+
+  containerForm: {
+    backgroundColor: '#fff',
+    flex: 1,
+    borderTopLeftRadius: 25,
+    borderTopRightRadius: 25,
+    paddingStart: '5%',
+    paddingEnd: '5%'
+  },
+
+})
